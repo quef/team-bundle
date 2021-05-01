df60484ca637b84706a99f625560c03ba2507fe0
Ulrich <1121867+metfan@users.noreply.github.com>
2021-05-01 18:53:23 +0200

    Fix symfony 4 deprecation
    Symfony 5 require to declare name when creating TreeBuilder.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,8 +18,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('quef_team');
+        $treeBuilder = new TreeBuilder('quef_team');
+        $rootNode = $treeBuilder->getRootNode();
 
         $rootNode->children()
             ->arrayNode('teams')
